56df74e6e3f8bdabd4f1bd0a578e9b0635a7ea3f
maokami <zerozerozero0216@gmail.com>
2024-12-22 16:35:03 +0900

    test: update main for testing

--- Main.lean
+++ Main.lean
@@ -1,4 +1,13 @@
 import VqcInLean
+import Mathlib.Data.Matrix.Basic
 
+def testMatrix : Matrix (Fin 2) (Fin 2) ℤ :=
+  ![![0, 1], ![2, -1]]
+
+-- Print a matrix element and hello message
+-- This is just a test
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  do
+    IO.println "Hello, world!"
+    IO.println "Matrix element at (0, 1):"
+    IO.println (toString (testMatrix 0 1))
